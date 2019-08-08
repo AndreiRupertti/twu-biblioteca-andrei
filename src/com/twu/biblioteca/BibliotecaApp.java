f27389b328db791820697373c864f315a12278b3
AndreiRupertti <arupertti@gmail.com>
2019-08-08 19:35:59 -0300

    feat: add strings resources and show welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,10 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.resources.Strings;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(Strings.get("app.welcomeMessage"));
     }
 }
---
+++ src/com/twu/biblioteca/resources/Strings.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca.resources;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+public class Strings {
+    private static final String STRINGS_BUNDLE = "strings.Strings";
+
+    public static String get(String key) {
+        return ResourceBundle.getBundle(STRINGS_BUNDLE).getString(key);
+    }
+}
